86f319fc5437857e25a646954e8e705a314cb0cd
maestro-blackbird <94738157+maestro-blackbird@users.noreply.github.com>
2021-11-20 04:05:23 -0500

    Update ContentView.swift

--- Poketch/ContentView.swift
+++ Poketch/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Welcome to the world of Pokémon!")
             .padding()
     }
 }
